a3b1598cb364dd5a43443803aa88406a7a2df331
Bonnieee17 <bonalynjane17@gmail.com>
2025-11-17 04:15:11 -0800

    Create MainActivity.kt with package declaration
    Add initial MainActivity.kt file with package declaration.

---
+++ MainActivity.kt
@@ -0,0 +1,2 @@
+echo package com.habomacha.rootsoflife > app\src\main\java\com\habomacha\rootsoflife\MainActivity.kt
+echo // full Kotlin code goes here >> app\src\main\java\com\habomacha\rootsoflife\MainActivity.kt
