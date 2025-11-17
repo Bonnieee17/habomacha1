3d7fb666440799d1ab0c59caff12c0ff7a17becf
Bonnieee17 <bonalynjane17@gmail.com>
2025-11-17 04:45:39 -0800

    Initialize MainActivity with package declaration

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,2 +1,4 @@
-echo package com.habomacha.rootsoflife > app\src\main\java\com\habomacha\rootsoflife\MainActivity.kt
-echo // full Kotlin code goes here >> app\src\main\java\com\habomacha\rootsoflife\MainActivity.kt
+package com.habomacha.rootsoflife
+
+
+class MainActivity {}
